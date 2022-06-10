557da4efa2276b40338eed9b2c418e0a881bba5c
vition vlad <48290199+VitionVlad@users.noreply.github.com>
2022-06-10 19:49:08 +0300

    Update CollWork.hpp

--- CollWork.hpp
+++ CollWork.hpp
@@ -18,7 +18,7 @@ class CollWork{
         return ((mx-high)*(mx-low) <= 0);
     }
     void vertexCollision(vec3 vertpos, vec3 &pos){
-        if(inRange(pos.x-0.1,pos.x+0.1, vertpos.x)&&inRange(pos.z-0.1,pos.z+0.1, vertpos.z)&&inRange(pos.y-1.8,pos.y+0.2, vertpos.y)){
+        if(inRange(pos.x-0.1,pos.x+0.1, vertpos.x)&&inRange(pos.z-0.1,pos.z+0.1, -vertpos.z)&&inRange(pos.y-1.8,pos.y+0.2, vertpos.y)){
             pos.y = lastpos.y;
             colide = true;
             if(vertpos.y > pos.y - 1.6){
@@ -31,7 +31,7 @@ class CollWork{
     }
     void calculateCollision(vec3 vertex[9999999], int totalvertex, vec3 &pos){
         totalvertex = totalvertex + 3;
-        for(int i = 1; i != totalvertex; i++){
+        for(int i = 0; i != totalvertex; i++){
             vertexCollision(vertex[i], pos);
         }
     }
