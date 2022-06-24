7f5d506fa99c4fae09e4fc7a4af1a595e127ed06
vition vlad <48290199+VitionVlad@users.noreply.github.com>
2022-06-24 19:41:45 +0300

    Update Example.cpp

--- Example.cpp
+++ Example.cpp
@@ -13,16 +13,18 @@ int main(){
     render.window = glfwCreateWindow(render.resolution.x, render.resolution.y, "", NULL, NULL);
     render.vshaderpath = "Engine/raw/vert.spv";
     render.fshaderpath = "Engine/raw/frag.spv";
+    render.pos.y = 10;
     Engine.Init();
-    Engine.plywork("App/Models/untitled.ply", 0, true);
+    Engine.plywork("App/Models/untitled.ply", 0, true, vec3(0, 0, 0));
+    Engine.plywork("App/Models/prop.ply", render.totalv, true, vec3(1, 3, -2));
     glfwSetInputMode(render.window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     double lastTime = glfwGetTime();
     int nbFrames = 0;
     string tittle;
     double currentTime;
     Engine.mouselook = true;
-    Engine.collisionenable = false;
-    Engine.enablephysics = false;
+    Engine.collisionenable = true;
+    Engine.enablephysics = true;
     while(!glfwWindowShouldClose(render.window)){
         glfwPollEvents();
         render.fov = usrfov;
