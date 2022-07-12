a9b881808e32a8481839cae403caf9602b29081d
vition vlad <48290199+VitionVlad@users.noreply.github.com>
2022-07-12 19:24:35 +0300

    Update CollWork.hpp

--- CollWork.hpp
+++ CollWork.hpp
@@ -13,6 +13,15 @@ class CollWork{
     vec3 lastpos;
     bool colide;
     bool allowmove = true;
+    float height = 1.8;
+    float uph = 0.2;
+    float allowdown = 0.2;
+    vec2 borders = vec2(0.1);
+    void physwork(vec3 &pos){
+        if(colide == false){
+            pos.y = pos.y - 0.1;
+        }
+    }
     void updateLastCoord(vec3 pos){
         lastpos = pos;
     }
@@ -20,10 +29,22 @@ class CollWork{
         return ((mx-high)*(mx-low) <= 0);
     }
     void vertexCollision(vec4 vertpos, vec3 &pos){
-        if(inRange(pos.x-0.1,pos.x+0.1, -vertpos.x)&&inRange(pos.z-0.1,pos.z+0.1, -vertpos.z)&&inRange(pos.y-1.8,pos.y+0.2, vertpos.y)){
+        if(inRange(pos.x-borders.x,pos.x+borders.x, -vertpos.x)&&inRange(pos.z-borders.y,pos.z+borders.y, -vertpos.z)&&inRange(pos.y-height,pos.y+uph, vertpos.y)){
             pos.y = lastpos.y;
             colide = true;
-            if(vertpos.y > pos.y - 1.6){
+            if(vertpos.y > pos.y - height + allowdown){
+                pos.x = lastpos.x;
+                pos.z = lastpos.z;
+            }
+        }else{
+            colide = false;
+        }
+    }
+    void vertexCollisionProp(vec4 vertpos, vec3 &pos){
+        if(inRange(pos.x-borders.x,pos.x+borders.x, vertpos.x)&&inRange(pos.z-borders.y,pos.z+borders.y, vertpos.z)&&inRange(pos.y-height,pos.y+uph, vertpos.y)){
+            pos.y = lastpos.y;
+            colide = true;
+            if(vertpos.y > pos.y - height + allowdown){
                 pos.x = lastpos.x;
                 pos.z = lastpos.z;
             }
@@ -32,9 +53,13 @@ class CollWork{
         }
     }
     void calculateCollision(vec4 *vertex, int totalvertex, vec3 &pos){
-        totalvertex = totalvertex + 3;
         for(int i = 0; i != totalvertex; i++){
             vertexCollision(vertex[i], pos);
+        }
+    }
+    void calculateCollisionProp(vec4 *vertex, int totalvertex, vec3 &pos){
+        for(int i = 0; i != totalvertex; i++){
+            vertexCollisionProp(vertex[i], pos);
         }
     }
 };
